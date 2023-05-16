9e279bd339458174de12fbfc3407f5958984a67e
Filip Huhta <filip.huhta@consid.se>
2023-05-16 16:24:35 +0200

    updated fetch url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Fetch the JSON file
-fetch('https://github.com/filiphuhta/coffe-quote/data/quotes.json')
+fetch('https://github.com/filiphuhta/coffe-quote/blob/main/data/quotes.json')
   .then(response => response.json())
   .then(data => {
     // Get the current date
